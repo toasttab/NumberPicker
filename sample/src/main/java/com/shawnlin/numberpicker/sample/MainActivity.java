0a983d276c1eb94c2b3226b2bed7bff6e7ed75dd
Shawn <hn62257@gmail.com>
2018-02-05 15:28:02 +0800

    New UI

--- sample/src/main/java/com/shawnlin/numberpicker/sample/MainActivity.java
+++ sample/src/main/java/com/shawnlin/numberpicker/sample/MainActivity.java
@@ -2,19 +2,21 @@ package com.shawnlin.numberpicker.sample;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
+import android.util.TypedValue;
+import android.widget.SeekBar;
 
+import com.larswerkman.lobsterpicker.OnColorListener;
+import com.larswerkman.lobsterpicker.sliders.LobsterShadeSlider;
 import com.shawnlin.numberpicker.NumberPicker;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "NumberPicker";
+    private static int MAX_VALUE_SEEK_BAR = 30;
+    private static int MIN_VALUE_SEEK_BAR = 16;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,68 +25,91 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NumberPicker numberPicker = findViewById(R.id.number_picker);
+        final NumberPicker numberPicker = findViewById(R.id.number_picker);
+        LobsterShadeSlider textColorSlider = findViewById(R.id.slider_text_color);
+        SeekBar textSizeSeekBar = findViewById(R.id.seek_bar_text_size);
+        LobsterShadeSlider selectedTextColorSlider = findViewById(R.id.slider_selected_text_color);
+        SeekBar selectedTextSizeSeekBar = findViewById(R.id.seek_bar_selected_text_size);
+        LobsterShadeSlider dividerColorSlider = findViewById(R.id.slider_divider_color);
 
-        // Set divider color
-        numberPicker.setDividerColor(ContextCompat.getColor(this, R.color.colorPrimary));
-        numberPicker.setDividerColorResource(R.color.colorPrimary);
+        textSizeSeekBar.setMax(MAX_VALUE_SEEK_BAR);
+        textSizeSeekBar.setProgress(MIN_VALUE_SEEK_BAR);
+        selectedTextSizeSeekBar.setMax(MAX_VALUE_SEEK_BAR);
+        selectedTextSizeSeekBar.setProgress(20);
 
-        // Set formatter
-        numberPicker.setFormatter(getString(R.string.number_picker_formatter));
         numberPicker.setFormatter(R.string.number_picker_formatter);
-
-        // Set selected text color
-        numberPicker.setSelectedTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
-        numberPicker.setSelectedTextColorResource(R.color.colorPrimary);
-
-        // Set selected text size
-        numberPicker.setSelectedTextSize(getResources().getDimension(R.dimen.selected_text_size));
-        numberPicker.setSelectedTextSize(R.dimen.selected_text_size);
-
-        // Set text color
-        numberPicker.setTextColor(ContextCompat.getColor(this, R.color.dark_grey));
-        numberPicker.setTextColorResource(R.color.dark_grey);
-
-        // Set text size
-        numberPicker.setTextSize(getResources().getDimension(R.dimen.text_size));
-        numberPicker.setTextSize(R.dimen.text_size);
-
-        // Set typeface
-        numberPicker.setTypeface(Typeface.create(getString(R.string.roboto_light), Typeface.NORMAL));
-        numberPicker.setTypeface(getString(R.string.roboto_light), Typeface.NORMAL);
-        numberPicker.setTypeface(getString(R.string.roboto_light));
         numberPicker.setTypeface(R.string.roboto_light, Typeface.NORMAL);
-        numberPicker.setTypeface(R.string.roboto_light);
 
-        // Set value
         numberPicker.setMaxValue(59);
         numberPicker.setMinValue(0);
         numberPicker.setValue(3);
 
-        // Set fading edge enabled
-        numberPicker.setFadingEdgeEnabled(true);
-
-        // Set scroller enabled
-        numberPicker.setScrollerEnabled(true);
-
-        // Set wrap selector wheel
-        numberPicker.setWrapSelectorWheel(true);
-
-        // OnClickListener
-        numberPicker.setOnClickListener(new View.OnClickListener() {
+        textSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View view) {
-                Log.d(TAG, "Click on current value");
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                numberPicker.setTextSize(spToPx((float) progress + MIN_VALUE_SEEK_BAR));
+                numberPicker.invalidate();
             }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-        // OnValueChangeListener
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        textColorSlider.addOnColorListener(new OnColorListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                Log.d(TAG, String.format(Locale.US, "oldVal: %d, newVal: %d", oldVal, newVal));
+            public void onColorChanged(@ColorInt int color) {
+                numberPicker.setTextColor(color);
+                numberPicker.invalidate();
             }
+
+            @Override
+            public void onColorSelected(@ColorInt int color) {}
         });
+
+        selectedTextSizeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                numberPicker.setSelectedTextSize(spToPx((float) progress + MIN_VALUE_SEEK_BAR));
+                numberPicker.invalidate();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        selectedTextColorSlider.addOnColorListener(new OnColorListener() {
+            @Override
+            public void onColorChanged(@ColorInt int color) {
+                numberPicker.setSelectedTextColor(color);
+                numberPicker.invalidate();
+            }
+
+            @Override
+            public void onColorSelected(@ColorInt int color) {}
+        });
+
+        dividerColorSlider.addOnColorListener(new OnColorListener() {
+            @Override
+            public void onColorChanged(@ColorInt int color) {
+                numberPicker.setDividerColor(color);
+                numberPicker.invalidate();
+            }
+
+            @Override
+            public void onColorSelected(@ColorInt int color) {}
+        });
+
+    }
+
+    private float spToPx(float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp,
+                getResources().getDisplayMetrics());
     }
 
 }
